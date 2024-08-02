c503ee1693b27a74ed1c65e15cf69f504a1c5d15
Jordy4200 <jordyvelasco3322@gmail.com>
2024-08-01 20:03:45 -0600

    si

--- src/main/java/com/spring_app/Entidad/Usuario.java
+++ src/main/java/com/spring_app/Entidad/Usuario.java
@@ -9,23 +9,30 @@ import lombok.Data;
 public class Usuario {
 
     @Size(min=3,max=10)
-    @NotBlank(message = "El nombre no puede estar vacío")
+    @NotBlank
     @Pattern(regexp = "[A-Za-z ]+", message = "El nombre solo puede contener letras y espacios")
     private String nombre;
 
-    @Size(min=3,max=10)
-    @NotBlank(message = "El nombre no puede estar vacío")
-    @Pattern(regexp = "[A-Za-z ]+", message = "El usuario solo puede contener letras y espacios")
+    @Size(min=3, max=10)
+    @NotBlank
+    @Pattern(regexp = "[A-Za-z0-9 ]+", message = "El usuario solo puede contener letras, espacios y números")
     private String usuario;
 
     @NotNull
     @Size(min=8,max=12)
-    @Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$", message = "La contraseña debe tener al menos 8 caracteres, una mayúscula, una minúscula, un número y un carácter especial")
+    @Pattern(regexp = "^" +
+            "(?=.*[a-z])" +
+            "(?=.*[A-Z])" +
+            "(?=.*\\d)" +
+            "(?=.*[@$!%*?&])" +
+            "[A-Za-z\\d@$!%*?&]{8,}$",
+            message = "La contraseña debe tener al menos 8 caracteres, una mayúscula, una minúscula, un número y un carácter especial")
     private String password;
 
 
+
+    @Email(message = "Correo real")
     @NotBlank
-    @Email(message = "Ingrese el  correo  electronico")
     private String email;
 
 
