9a1f4819eca377b38855c63888158f051ebea7c5
Jordy4200 <jordyvelasco3322@gmail.com>
2024-08-01 18:45:30 -0600

    registro

---
+++ src/main/java/com/spring_app/Controlador/RegistroControlador.java
@@ -0,0 +1,31 @@
+package com.spring_app.Controlador;
+
+
+import com.spring_app.Entidad.Usuario;
+import jakarta.validation.Valid;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class RegistroControlador {
+
+    @GetMapping("/registro")
+    public String formularioAutor(Model model){
+        model.addAttribute("usuario",new Usuario());
+        return  "registro";
+    }
+    @PostMapping("/registrarUsuario")
+    public String registrarUsuario(@Valid @ModelAttribute Usuario usuario, BindingResult bindingResult, Model model){
+        if(bindingResult.hasErrors()){
+            model.addAttribute("errors",bindingResult.getAllErrors());
+            return "registro";
+        }else{
+
+            return "registro";
+        }
+    }
+}
---
+++ src/main/java/com/spring_app/Entidad/Usuario.java
@@ -0,0 +1,34 @@
+package com.spring_app.Entidad;
+
+
+import jakarta.validation.constraints.*;
+import lombok.Data;
+
+
+@Data
+public class Usuario {
+
+    @Size(min=3,max=10)
+    @NotBlank(message = "El nombre no puede estar vacío")
+    @Pattern(regexp = "[A-Za-z ]+", message = "El nombre solo puede contener letras y espacios")
+    private String nombre;
+
+    @Size(min=3,max=10)
+    @NotBlank(message = "El nombre no puede estar vacío")
+    @Pattern(regexp = "[A-Za-z ]+", message = "El nombre solo puede contener letras y espacios")
+    private String usuario;
+
+    @NotNull
+    @Size(min=8,max=12)
+    @Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$", message = "La contraseña debe tener al menos 8 caracteres, una mayúscula, una minúscula, un número y un carácter especial")
+    private String password;
+
+
+    @NotBlank
+    @Email(message = "Ingrese el  correo  electronico")
+    private String email;
+
+
+
+
+}
