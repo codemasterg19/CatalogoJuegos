03f0d182c6cc6fd8e84d8f43e2f64a7b889c790e
pabloj19 <pablo_jmz1@hotmail.com>
2024-08-07 12:46:16 -0500

    crud formulario

--- src/main/java/com/spring_app/Repositorio/JuegoRepositorio.java
+++ src/main/java/com/spring_app/Repositorio/JuegoRepositorio.java
@@ -6,6 +6,11 @@ import com.spring_app.Entidad.Juego;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface JuegoRepositorio extends JpaRepository<Juego, Long> {
+
+    // Método para buscar juegos que contienen una cadena en su nombre (usando LIKE)
+  /*  List<Juego> findByNombreContaining(String nombre);*/
 }
--- src/main/java/com/spring_app/Servicio/JuegoServicio.java
+++ src/main/java/com/spring_app/Servicio/JuegoServicio.java
@@ -1,2 +1,41 @@
-package com.spring_app.Servicio;public class JuegoServicio {
+package com.spring_app.Servicio;
+
+import com.spring_app.Entidad.Juego;
+import com.spring_app.Repositorio.JuegoRepositorio;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class JuegoServicio {
+    @Autowired
+    private JuegoRepositorio juegoRepositorio;
+
+   /* // Método para buscar juegos por nombre que contienen una cadena
+    public List<Juego> buscarJuegosPorNombre(String nombre) {
+        return juegoRepositorio.findByNombreContaining(nombre);
+    }*/
+
+    //Leer
+    public List<Juego> listarJuegos(){
+        return juegoRepositorio.findAll();
+    }
+
+    public Optional<Juego> buscarJuego(Long id){
+        return juegoRepositorio.findById(id);
+    }
+
+    public void guardarJuego(Juego juego){
+
+        juegoRepositorio.save(juego);
+    }
+
+    public void eliminarJuego(Long id){
+        juegoRepositorio.deleteById(id);
+    }
+
+
+
 }
