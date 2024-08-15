27fd7c2ae769a4cc7f1c68d1762f01d39b2d49f5
Jordy4200 <jordyvelasco3322@gmail.com>
2024-08-14 19:57:00 -0600

    Shi

--- src/main/java/com/spring_app/Taller1Application.java
+++ src/main/java/com/spring_app/Taller1Application.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Taller1Application {
+public class   Taller1Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Taller1Application.class, args);
